94d6ebb20d447bc025d6a59581e98a384be9dbb4
Pankaj Bhambhani <pankajb64@gmail.com>
2015-03-28 17:11:30 +0530

    Created Final Result Fragment

--- app/src/main/java/com/fl/dlc/FinalResultFragment.java
+++ app/src/main/java/com/fl/dlc/FinalResultFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Team1DetailsFragment.OnFragmentInteractionListener} interface
+ * {@link FinalResultFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Team1DetailsFragment#newInstance} factory method to
+ * Use the {@link FinalResultFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Team1DetailsFragment extends Fragment {
+public class FinalResultFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class Team1DetailsFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Team1DetailsFragment() {
+    public FinalResultFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class Team1DetailsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Team1DetailsFragment.
+     * @return A new instance of fragment FinalResultFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Team1DetailsFragment newInstance(String param1, String param2) {
-        Team1DetailsFragment fragment = new Team1DetailsFragment();
+    public static FinalResultFragment newInstance(String param1, String param2) {
+        FinalResultFragment fragment = new FinalResultFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class Team1DetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team1_details, container, false);
+        return inflater.inflate(R.layout.fragment_final_result, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
