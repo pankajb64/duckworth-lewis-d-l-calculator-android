df5012c967555e73461de3e7465582198c795138
Pankaj Bhambhani <pankajb64@gmail.com>
2015-04-14 01:01:53 +0530

    Changed App Rater day settings back to default

--- app/src/main/java/com/fl/dlc/util/AppRater.java
+++ app/src/main/java/com/fl/dlc/util/AppRater.java
@@ -14,8 +14,8 @@ public class AppRater {
     private final static String APP_TITLE = "Duckworth Lewis (D/L) Calculator";
     private final static String APP_PNAME = "com.fl.dlc";
 
-    private final static int DAYS_UNTIL_PROMPT = 0;
-    private final static int LAUNCHES_UNTIL_PROMPT = 0;
+    private final static int DAYS_UNTIL_PROMPT = 3;
+    private final static int LAUNCHES_UNTIL_PROMPT = 7;
 
     public static void app_launched(Context mContext) {
         SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
